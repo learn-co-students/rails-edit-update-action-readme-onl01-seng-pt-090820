a3355136f7da51c6359e142b38ce720c54170f9a
DavidRMorphew <morphewd@umich.edu>
2021-01-12 17:46:56 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    # binding.pry
     @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
